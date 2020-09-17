17b96d54828d7cab9c6c89959053c48f9f6a05f6
redsnowstorm <69949050+redsnowstorm@users.noreply.github.com>
2020-09-17 04:35:41 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+// document.querySelector("img.img1").setAttributeNode("src", "/images/randomImage1;");
+
+function DiceRoll() {
+
+    let randomNumber1 = Math.floor(Math.random()*6)+1;
+    document.querySelectorAll("img")[0].setAttribute("src", "/images/dice"+randomNumber1+".PNG");
+    
+    let randomNumber2 = Math.floor(Math.random()*6)+1;
+    document.querySelectorAll("img")[1].setAttribute("src", "/images/dice"+randomNumber2+".PNG");
+
+    if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 wins!<br>🏆"
+    }
+
+    if(randomNumber1<randomNumber2){
+        document.querySelector("h1").innerHTML = "Player 2 wins!<br>🥇"
+            }
+
+    if(randomNumber1 === randomNumber2){
+    document.querySelector("h1").innerHTML = "Draw!<br>🤼‍♂️"
+    }
+}
+    
+    DiceRoll();
+
+    
