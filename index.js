74f15c46c56c90439fd27e5bacea69bc617acc52
redsnowstorm <69949050+redsnowstorm@users.noreply.github.com>
2020-09-17 05:02:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 function DiceRoll() {
 
     let randomNumber1 = Math.floor(Math.random()*6)+1;
-    document.querySelectorAll("img")[0].setAttribute("src", "/images/dice"+randomNumber1+".PNG");
+    document.querySelectorAll("img")[0].setAttribute("src", "images/dice"+randomNumber1+".PNG");
     
     let randomNumber2 = Math.floor(Math.random()*6)+1;
-    document.querySelectorAll("img")[1].setAttribute("src", "/images/dice"+randomNumber2+".PNG");
+    document.querySelectorAll("img")[1].setAttribute("src", "images/dice"+randomNumber2+".PNG");
 
     if(randomNumber1>randomNumber2){
     document.querySelector("h1").innerHTML = "Player 1 wins!<br>🏆"
